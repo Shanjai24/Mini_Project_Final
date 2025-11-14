1c29bb4bd29ffd3b9a85c1b3368c31156024be66
Shanjai24 <shanjaimr245@gmail.com>
2025-11-14 13:02:37 +0530

    ml deploy

---
+++ ml/main.py
@@ -0,0 +1,7 @@
+import os
+from api_server import app
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("api_server:app", host="0.0.0.0", port=port, reload=False)
